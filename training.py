136fbc99295fc61e04508b9d15c2a4bef2349a93
yahya <snikoue1@binghamton.edu>
2018-11-26 17:23:05 -0500

    reading the dataset with pandas

--- training.py
+++ training.py
@@ -1,22 +1,36 @@
-
 from pycocotools.coco import COCO
-import pandas as pd
-import json
-
 import numpy as np
-
 import matplotlib.pyplot as plt
 import pylab
 
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 dataDir='..'
 dataType='val2017'
+annFile = '../The_Pose/database/coco/annotations/instances_train2017.json'.format(dataDir,dataType)
+coco=COCO(annFile)
+
+cats = coco.loadCats(coco.getCatIds())
+nms=[cat['name'] for cat in cats]
+print('COCO categories: \n{}\n'.format(' '.join(nms)))
+
+nms = set([cat['supercategory'] for cat in cats])
+print('COCO supercategories: \n{}'.format(' '.join(nms)))
+catIds = coco.getCatIds(catNms=['person','dog','skateboard'])
+imgIds = coco.getImgIds(catIds=catIds );
+imgIds = coco.getImgIds(imgIds = [324158])
+img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+I = io.imread(img['coco_url'])
+plt.axis('off')
+plt.imshow(I)
+plt.show()
+plt.imshow(I); plt.axis('off')
+annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+anns = coco.loadAnns(annIds)
+coco.showAnns(anns)
 annFile = '../The_Pose/database/coco/annotations/person_keypoints_val2017.json'.format(dataDir,dataType)
 coco_kps=COCO(annFile)
-
-with open(annFile) as data_file:
-    data = json.load(data_file)
-
-print data
-
-
-# print labels
+plt.imshow(I); plt.axis('off')
+ax = plt.gca()
+annIds = coco_kps.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+anns = coco_kps.loadAnns(annIds)
+coco_kps.showAnns(anns)
