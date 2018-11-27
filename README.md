d91cc92320fab0f4cfefee67a4fc9aafcaf128da
yahya <snikoue1@binghamton.edu>
2018-11-27 16:41:08 -0500

    reading the anns from the dataset

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ In order to read the COCO imageset annotations for the key_point detection, we i
 For this installation first the execute "make" under cocoAPI/PythonAPI and then run the command "Sudo setup.py install". This way the library os added to the python path and is reached outside of the library.
 
 
+
--- training.py
+++ training.py
@@ -2,35 +2,16 @@ from pycocotools.coco import COCO
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab
+import skimage.io as io
 
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 dataDir='..'
 dataType='val2017'
-annFile = '../The_Pose/database/coco/annotations/instances_train2017.json'.format(dataDir,dataType)
-coco=COCO(annFile)
+coco_kps=COCO('../The_Pose/database/coco/annotations/person_keypoints_train2017.json'.format(dataDir,dataType))
+imgIds = coco_kps.getImgIds(catIds=coco_kps.getCatIds(catNms=['person']))
 
-cats = coco.loadCats(coco.getCatIds())
-nms=[cat['name'] for cat in cats]
-print('COCO categories: \n{}\n'.format(' '.join(nms)))
-
-nms = set([cat['supercategory'] for cat in cats])
-print('COCO supercategories: \n{}'.format(' '.join(nms)))
-catIds = coco.getCatIds(catNms=['person','dog','skateboard'])
-imgIds = coco.getImgIds(catIds=catIds );
-imgIds = coco.getImgIds(imgIds = [324158])
-img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
-I = io.imread(img['coco_url'])
-plt.axis('off')
-plt.imshow(I)
-plt.show()
-plt.imshow(I); plt.axis('off')
-annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-anns = coco.loadAnns(annIds)
-coco.showAnns(anns)
-annFile = '../The_Pose/database/coco/annotations/person_keypoints_val2017.json'.format(dataDir,dataType)
-coco_kps=COCO(annFile)
-plt.imshow(I); plt.axis('off')
-ax = plt.gca()
-annIds = coco_kps.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-anns = coco_kps.loadAnns(annIds)
-coco_kps.showAnns(anns)
+for i in range (1,2):
+    img = coco_kps.loadImgs(imgIds[i])[0]
+    annIds = coco_kps.getAnnIds(imgIds=img['id'])
+    anns = coco_kps.loadAnns(annIds)
+    print anns
