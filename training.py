84456bd6267f59edb1b014b4a45fca038d7b0d90
yahya <snikoue1@binghamton.edu>
2018-11-28 15:36:29 -0500

    data read and nomilaziation finished

--- training.py
+++ training.py
@@ -1,3 +1,5 @@
+
+from __future__ import division
 from pycocotools.coco import COCO
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,10 +10,37 @@ pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 dataDir='..'
 dataType='val2017'
 coco_kps=COCO('../The_Pose/database/coco/annotations/person_keypoints_train2017.json'.format(dataDir,dataType))
-imgIds = coco_kps.getImgIds(catIds=coco_kps.getCatIds(catNms=['person']))
+catIds = coco_kps.getCatIds(catNms=['person'])
 
+
+cats = coco_kps.loadCats(coco_kps.getCatIds())
+print cats[0]
+
+
+nms=[cat['name'] for cat in cats]
+imgIds = coco_kps.getImgIds(catIds=coco_kps.getCatIds(catNms=['person']))
 for i in range (1,2):
     img = coco_kps.loadImgs(imgIds[i])[0]
+    x =  img['width']
+    y = img['height']
     annIds = coco_kps.getAnnIds(imgIds=img['id'])
-    anns = coco_kps.loadAnns(annIds)
-    print anns
+    anns = coco_kps.loadAnns(annIds)[0]
+    print anns['keypoints']
+    anns['keypoints'][0] /= x
+    anns['keypoints'][15] /= x
+    anns['keypoints'][18] /= x
+    anns['keypoints'][21] /= x
+    anns['keypoints'][24] /= x
+    anns['keypoints'][1] /= y
+    anns['keypoints'][16] /= y
+    anns['keypoints'][19] /= y
+    anns['keypoints'][22] /= y
+    anns['keypoints'][25] /= y
+    print anns['keypoints']
+    keypoints = np.array(anns['keypoints'])
+
+
+
+
+
+
