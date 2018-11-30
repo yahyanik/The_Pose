0c46267075d99e9fd6d852ba87f461cbd97b1508
yahya <snikoue1@binghamton.edu>
2018-11-30 15:16:13 -0500

    data read finished. ready for transforming.

---
+++ test.py
@@ -0,0 +1,38 @@
+
+from __future__ import division
+from pycocotools.coco import COCO
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import skimage.io as io
+
+
+#
+# pylab.rcParams['figure.figsize'] = (8.0, 10.0)
+# dataDir='..'
+# dataType='val2017'
+# coco=COCO('../The_Pose/database/coco/annotations/instances_train2017.json'.format(dataDir,dataType))
+# catIds = coco.getCatIds(catNms=['person'])
+#
+#
+# catIds = coco.getCatIds(catNms=['person']);
+# imgIds = coco.getImgIds(catIds=catIds );
+# img = coco.loadImgs(imgIds[6])[0]
+#
+# I = io.imread(img['coco_url'])
+# plt.axis('off')
+# plt.imshow(I)
+# plt.show()
+#
+# plt.imshow(I); plt.axis('off')
+# annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+# anns = coco.loadAnns(annIds)
+# coco.showAnns(anns)
+# annFile = '../The_Pose/database/coco/annotations/person_keypoints_train2017.json'.format(dataDir,dataType)
+# coco_kps=COCO(annFile)
+# plt.imshow(I); plt.axis('off')
+# ax = plt.gca()
+# annIds = coco_kps.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+# anns = coco_kps.loadAnns(annIds)
+# coco_kps.showAnns(anns)
+# print 'kk'
--- training.py
+++ training.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import pylab
 import skimage.io as io
 
+
+
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 dataDir='..'
 dataType='val2017'
@@ -14,30 +16,45 @@ catIds = coco_kps.getCatIds(catNms=['person'])
 
 
 cats = coco_kps.loadCats(coco_kps.getCatIds())
-print cats[0]
+# print cats[0]
 
 
 nms=[cat['name'] for cat in cats]
 imgIds = coco_kps.getImgIds(catIds=coco_kps.getCatIds(catNms=['person']))
-for i in range (1,2):
+
+for i in range (6,7):
     img = coco_kps.loadImgs(imgIds[i])[0]
     x =  img['width']
+    I = io.imread(img['coco_url'])
     y = img['height']
     annIds = coco_kps.getAnnIds(imgIds=img['id'])
-    anns = coco_kps.loadAnns(annIds)[0]
-    print anns['keypoints']
-    anns['keypoints'][0] /= x
-    anns['keypoints'][15] /= x
-    anns['keypoints'][18] /= x
-    anns['keypoints'][21] /= x
-    anns['keypoints'][24] /= x
-    anns['keypoints'][1] /= y
-    anns['keypoints'][16] /= y
-    anns['keypoints'][19] /= y
-    anns['keypoints'][22] /= y
-    anns['keypoints'][25] /= y
-    print anns['keypoints']
-    keypoints = np.array(anns['keypoints'])
+    # print annIds
+    label = np.zeros(10,10,15)
+    for id in annIds:
+        anns = coco_kps.loadAnns(id)
+        # print anns
+        key = anns[0]['keypoints']
+
+        key[0] /= x
+        key[15] /= x
+        key[18] /= x
+        key[21] /= x
+        key[24] /= x
+        key[1] /= y
+        key[16] /= y
+        key[19] /= y
+        key[22] /= y
+        key[25] /= y
+    # print anns['keypoints']
+    # keypoints = np.array([key[0],key[1],key[15],anns['keypoints'][16],anns['keypoints'][18],anns['keypoints'][19],anns['keypoints'][21],anns['keypoints'][22],anns['keypoints'][24],anns['keypoints'][25]])
+
+
+
+
+
+
+
+
 
 
 
